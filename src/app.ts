c2d5cb57c42aca95f9f02f441ad0dbdce61f148b
Yunbeom Cha <uiurihappy@sju.ac.kr>
2022-03-11 19:22:51 +0900

    feat: cat mocking

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,14 @@
-console.log("hello world!!");
+import * as express from "express";
+import { Cat, CatType } from "./app.model";
+
+const app: express.Express = express();
+const data = [1, 2, 3, 4];
+
+app.get("/", (req: express.Request, res: express.Response) => {
+  console.log(req);
+  res.send({ cats: Cat });
+});
+
+app.listen(8000, () => {
+  console.log("Server is on....");
+});
